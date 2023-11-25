1a0b5c3950e14de8efa2a5643fab56d75675c657
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-11-25 19:09:04 +0600

    Implement Firebase

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,23 +6,17 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey:import.meta.env.VITE_APIKEY,
-  // authDomain:import.meta.env.VITE_AUTHDOMAIN,
-  // projectId:import.meta.env.VITE_PROJECTID,
-  // storageBucket:import.meta.env.VITE_STORAGEBUCKET,
-  // messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
-  // appId:import.meta.env.VITE_APPID,
-
-
-  apiKey: "AIzaSyCpkmIprIC_z6Zk8UvscoiubiaI8tpGVeQ",
-  authDomain: "set-up-projects.firebaseapp.com",
-  projectId: "set-up-projects",
-  storageBucket: "set-up-projects.appspot.com",
-  messagingSenderId: "27845294488",
-  appId: "1:27845294488:web:65b919fafecb65603953ed"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
 
 export default app;
