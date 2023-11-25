65748869763607fcb9c0a86ea9c63919eceb1d78
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-11-25 17:46:58 +0600

    Implement AddArticle Form Page

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar fixed z-10 bg-opacity-60 bg-black text-white mx-auto">
+    <div className="navbar top-0 fixed z-10 bg-opacity-60 bg-black text-white mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- src/page/AddArticle/AddArticle.jsx
+++ src/page/AddArticle/AddArticle.jsx
@@ -1,142 +1,154 @@
-import { useContext, useEffect } from 'react';
-import { AuthContext } from '../../providers/AuthProvider';
-
-
+import Select from "react-select";
+import swal from "sweetalert";
 
 const AddArticle = () => {
-    const { user } = useContext(AuthContext);
+  const tags = [
+    { value: "#fashion", label: "#fashion" },
+    { value: "#sports", label: "#sports" },
+    { value: "#fitness", label: "#fitness" },
+    { value: "#movie", label: "#movie" },
+    { value: "#travel", label: "#travel" },
+    { value: "#magazine", label: "#magazine" },
+  ];
+  const publisher = [
+    { value: "Daily Star", label: "Daily Star" },
+    { value: "The Independent", label: "The Independent" },
+    { value: "New Age", label: "New Age" },
+    { value: "Dhaka Tribune", label: "Dhaka Tribune" },
+  ];
 
-    useEffect(() => {
-        document.title = 'HomeSwap | Add Service'
-      },[])
+  const handleAddProduct = (event) => {
+    event.preventDefault();
 
+    const form = event.target;
 
-    const handleAddProduct = event => {
-        event.preventDefault();
+    const title = form.title.value;
+    const publisher = form.publisher.value;
+    const tags = form.tags.value;
+    const description = form.description.value;
+    const image = form.image.value;
 
-        const form = event.target;
+    const newProduct = { title, publisher, tags, description, image };
 
-        const userName = user ? user.displayName : form.userName.value;
-        const email = user ? user.email : form.email.value;
-        const serviceName = form.serviceName.value;
-        const price = form.price.value;
-        const description = form.description.value;
-        const image = form.image.value;
-        const area = form.area.value;
+    console.log(newProduct);
+    console.log(tags);
 
-        const newProduct = { userName, serviceName, email, price, description, image, area }
+    // send data to the server
+    fetch("https://ass10-two.vercel.app/product", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          swal("Success!", "Product Added Successfully", "success");
+        }
+      });
+  };
 
-        console.log(newProduct); 
-
-        // send data to the server
-   
-    }
-
-
-    return (
-        <div className="bg-base-300 rounded-xl p-14 py-20 max-w-5xl mx-auto mb-16">
+  return (
+    <div className="mt-20 py-10">
+      <div className="bg-base-300 rounded-xl my-20 p-14 py-20 max-w-5xl mx-auto">
         <div className="text-center mb-16">
-        <h2 className="text-3xl font-extrabold pb-8">Add New Service</h2>
-        <p><span className="text-red-500">HomeSwap Hub </span>is a user-friendly online platform designed to facilitate home swapping, making it easy for homeowners to exchange their residences with others.</p>
+          <h2 className="text-3xl font-extrabold pb-8">Add New Article</h2>
+          <p>
+            <span className="text-red-500">News12Paper</span>. Explore a World
+            of News: Uncover the Latest Headlines, In-Depth Features, and
+            Thought-Provoking Analysis on our Comprehensive Newspaper Article
+            Website
+          </p>
         </div>
         <form onSubmit={handleAddProduct}>
-            {/* name and Brand name Row */}
-            {user ? (
-                    <div className="md:flex mb-8">
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text">Article Title</span>
-                            </label>
-                            <label className="input-group">
-                                <input type="text" name="userName" value={user.displayName} readOnly className="input input-bordered w-full" />
-                            </label>
-                        </div>
-                        <div className="form-control md:w-1/2 md:ml-4">
-                            <label className="label">
-                                <span className="label-text">User Email</span>
-                            </label>
-                            <label className="input-group">
-                                <input type="email" name="email" value={user.email} readOnly className="input input-bordered w-full" />
-                            </label>
-                        </div>
-                    </div>
-                ) : (
-                    <>
-                        <div className="md:flex mb-8">
-                            <div className="form-control md:w-1/2">
-                                <label className="label">
-                                    <span className="label-text">User Name</span>
-                                </label>
-                                <label className="input-group">
-                                    <input type="text" name="userName" placeholder="User Name" className="input input-bordered w-full" />
-                                </label>
-                            </div>
-                            <div className="form-control md:w-1/2 md:ml-4">
-                                <label className="label">
-                                    <span className="label-text">User Email</span>
-                                </label>
-                                <label className="input-group">
-                                    <input type="email" name="email" placeholder="Email" className="input input-bordered w-full" />
-                                </label>
-                            </div>
-                        </div>
-                    </>
-                )}
-            {/* type & price row */}
-            <div className="md:flex mb-8">
-                <div className="form-control md:w-1/2">
-                <label className="label">
-                        <span className="label-text">Service Name</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="serviceName" placeholder="Service Name" className="input input-bordered w-full" />
-                    </label>
-                    
-                </div>
-                <div className="form-control md:w-1/2 md:ml-4">
-                    <label className="label">
-                        <span className="label-text">Price</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="price" placeholder="$ Price" className="input input-bordered w-full" />
-                    </label>
-                </div>
+          {/* name and Brand name Row */}
+          <div className="md:flex mb-8">
+            <div className="form-control md:w-1/2">
+              <label className="label">
+                <span className="label-text">Title</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Article Title"
+                  className="input input-bordered w-full"
+                />
+              </label>
             </div>
-            {/* descriptions & rating row */}
-            <div className="md:flex mb-8">
-                <div className="form-control md:w-1/2">
-                    <label className="label">
-                        <span className="label-text">Description</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="description" placeholder="Description" className="input input-bordered w-full" />
-                    </label>
-                </div>
-                <div className="form-control md:w-1/2 md:ml-4">
-                    <label className="label">
-                        <span className="label-text">Service Area</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="area" placeholder="Service Area" className="input input-bordered w-full" />
-                    </label>
-                </div>
-            </div>
-            {/* image row */}
-            <div className="mb-8">
-            <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Service Image URL</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="image" placeholder="Image URL" className="input input-bordered w-full" />
-                    </label>
-                </div>
-            </div>
-            <input type="submit" value="Add Service" className="btn btn-block bg-slate-800 text-white" />
+            <div className="form-control md:w-1/2 md:ml-4">
+            <label className="label">
+                <span className="label-text">Description</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="description"
+                  placeholder="Description"
+                  className="input input-bordered w-full"
+                />
+              </label>
 
+            </div>
+          </div>
+          {/* type & price row */}
+          <div className="md:flex mb-8">
+            <div className="form-control md:w-1/2">
+              <label className="label">
+                <span className="label-text">Tags</span>
+              </label>
+              <label className="input-group">
+                <Select
+                  options={tags}
+                  isMulti
+                  name="tags"
+                  className="basic-multi-select py-2 w-full"
+                  classNamePrefix="select"
+                />
+              </label>
+            </div>
+            <div className="form-control md:w-1/2 md:ml-4">
+            <label className="label">
+                <span className="label-text">Publisher</span>
+              </label>
+              <label className="input-group">
+              <Select
+                  options={publisher}
+                  isMulti
+                  name="publisher"
+                  className="basic-multi-select py-2 w-full"
+                  classNamePrefix="select"
+                />
+              </label>
+            </div>
+          </div>
+          {/* image row */}
+          <div className="mb-8">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Image URL</span>
+              </label>
+              <label className="input-group">
+                <input
+                  type="text"
+                  name="image"
+                  placeholder="Image URL"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+          </div>
+          <input
+            type="submit"
+            value="Add Product"
+            className="btn btn-block bg-slate-800 text-white"
+          />
         </form>
+      </div>
     </div>
-);
+  );
 };
 
 export default AddArticle;
