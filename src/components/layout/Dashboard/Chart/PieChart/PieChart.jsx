86c60652127d7bfcfab3509437506656070c719d
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-11-27 21:03:15 +0600

    Implement Helmate

--- src/components/layout/Dashboard/Chart/PieChart/PieChart.jsx
+++ src/components/layout/Dashboard/Chart/PieChart/PieChart.jsx
@@ -16,18 +16,17 @@ export const options = {
   title: "Pie Chart",
 };
 
-
 const PieChart = () => {
   return (
     <Container>
       <div>
-      <Chart
-      chartType="PieChart"
-      data={data}
-      options={options}
-      width={"100%"}
-      height={"400px"}
-    />
+        <Chart
+          chartType="PieChart"
+          data={data}
+          options={options}
+          width={"100%"}
+          height={"400px"}
+        />
       </div>
     </Container>
   );
